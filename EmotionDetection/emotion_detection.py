228e3f0064cfa4b2dd987d0ec7f146fa73947ce6
Tanawat Limungkura <122532279+bopiaknugget@users.noreply.github.com>
2025-03-20 15:12:05 +0700

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,50 +1,23 @@
-from flask import Flask, json
 import requests
-def error_handling_function(text) : 
-    if text == "" or isinstance(text, type(None)) :
-        return True
-    return False
-
+from flask import json
+ 
 def emotion_detector(text_to_analyze):
 
-    if(error_handling_function(text_to_analyze)) :
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyze} }
 
-        result = {
-            'anger': None,
-            'disgust': None,
-            'fear': None,
-            'joy': None,
-            'sadness': None,
-            'dominant_emotion': None
-        }
-        return json.dumps(result, indent=4)
+    response = requests.post(url, json=myobj, headers=headers)
 
-    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    
-    headers = {
-        "Content-Type": "application/json",
-        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
-    }
+    if response.status_code == 400 :
+        emotions = {'anger': None,'disgust': None,
+                    'fear': None,'joy': None,
+                    'sadness': None,
+                    'dominant_emotion': None}
+    elif response.status_code == 200:
+        response = response.text
+        data =  json.loads(response)
+        emotions = data["emotionPredictions"][0]["emotion"]
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
 
-    data = {
-        "raw_document": {"text": text_to_analyze}
-    }
-
-    
-
-
-    response = requests.post(url, headers=headers, json=data).json()
-    emotions = response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotions, key=emotions.get)
-    
-    result = {
-        "anger": emotions["anger"],
-        "disgust": emotions["disgust"],
-        "fear": emotions["fear"],
-        "joy": emotions["joy"],
-        "sadness": emotions["sadness"],
-        "dominant_emotion": dominant_emotion
-    }
-
-    return json.dumps(result, indent=4)
-
+    return emotions
